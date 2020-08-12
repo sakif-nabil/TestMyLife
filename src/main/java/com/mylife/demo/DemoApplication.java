f171ad8dc0bea7c9eff3b6f2cf915a3a0044f822
sakif-nabil <53219441+sakif-nabil@users.noreply.github.com>
2020-08-12 17:53:52 +0100

    add core

--- src/main/java/com/mylife/demo/DemoApplication.java
+++ src/main/java/com/mylife/demo/DemoApplication.java
@@ -1,11 +1,19 @@
 package com.mylife.demo;
 
+import com.mylife.demo.dao.ProduitRepository;
+import com.mylife.demo.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+    @Autowired
+    private ProduitRepository produitRepository;
+    @Autowired
+    private RepositoryRestConfiguration restConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -13,6 +21,20 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        restConfiguration.exposeIdsFor(Produit.class);
+        produitRepository.save(new Produit(null,"lenovo 1",1,10));
+        produitRepository.save(new Produit(null,"lenovo 2",2,20));
+        produitRepository.save(new Produit(null,"lenovo 3",3,30));
+
+
+        System.out.println("hello");
+        produitRepository.findAll().forEach(p->{
+            System.out.println(p.toString());
+
+
+        });
+        System.out.println("hello worldddddddddd");
+        System.out.println("hello l3alaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam");
         System.out.println("hi l3alaaaaaaaaaaaaaaaaaaaaaaaaam");
     }
 }
